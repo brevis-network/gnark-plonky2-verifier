84542003a12c1c0959fb06df6f279c988815096a
liuxiaobleach <1241368737@qq.com>
2024-08-23 21:12:27 +0800

    update

--- brevis-agg/utils/tools.go
+++ brevis-agg/utils/tools.go
@@ -9,43 +9,47 @@ import (
 func GetReceiptData(receipts []*proof.SDKQueryProvingInfoForReceipt) [][]uint64 {
 	var result [][]uint64
 	for _, receipt := range receipts {
-		var res []uint64
-		for _, logInfo := range receipt.LogExtractInfos {
-			contractFields := parseHexStringToField(logInfo.ContractAddress)
-
-			for len(contractFields) < 3 {
-				contractFields = append(contractFields, 0)
-			}
-			reverseUint64Slice(contractFields)
-
-			res = append(res, contractFields...)
-
-			eventIDFields := parseHexStringToField(logInfo.LogTopic0[:14])
-			res = append(res, eventIDFields...)
-
-			if logInfo.ValueFromTopic {
-				res = append(res, 1)
-			} else {
-				res = append(res, 0)
-			}
-
-			res = append(res, uint64(logInfo.ValueIndex))
-
-			valueFields := parseHexStringToField(logInfo.Value)
-			for len(valueFields) < 4 {
-				valueFields = append(valueFields, 0)
-			}
-			reverseUint64Slice(valueFields)
-			res = append(res, valueFields...)
-		}
-
-		for len(res) < 10*8 {
-			res = append(res, 0)
-		}
-		res = append([]uint64{receipt.BlockNumber}, res...)
-		result = append(result, res)
+		result = append(result, GetOneReceiptData(receipt))
 	}
 	return result
+}
+
+func GetOneReceiptData(receipt *proof.SDKQueryProvingInfoForReceipt) []uint64 {
+	var res []uint64
+	for _, logInfo := range receipt.LogExtractInfos {
+		contractFields := parseHexStringToField(logInfo.ContractAddress)
+
+		for len(contractFields) < 3 {
+			contractFields = append(contractFields, 0)
+		}
+		reverseUint64Slice(contractFields)
+
+		res = append(res, contractFields...)
+
+		eventIDFields := parseHexStringToField(logInfo.LogTopic0[:14])
+		res = append(res, eventIDFields...)
+
+		if logInfo.ValueFromTopic {
+			res = append(res, 1)
+		} else {
+			res = append(res, 0)
+		}
+
+		res = append(res, uint64(logInfo.ValueIndex))
+
+		valueFields := parseHexStringToField(logInfo.Value)
+		for len(valueFields) < 4 {
+			valueFields = append(valueFields, 0)
+		}
+		reverseUint64Slice(valueFields)
+		res = append(res, valueFields...)
+	}
+
+	for len(res) < 10*8 {
+		res = append(res, 0)
+	}
+	res = append([]uint64{receipt.BlockNumber}, res...)
+	return res
 }
 
 func reverseUint64Slice(slice []uint64) {
